79af14690b5f26be5838f4bff18b8e765c72dd11
exe544 <a.lashko97@gmail.com>
2023-11-23 19:49:35 +0200

    add IsAdmin middleware to companies route resource

---
+++ app/Http/Middleware/IsAdminMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\Role;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsAdminMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        abort_if($request->user()->role_id !== Role::ADMINISTRATOR->value, Response::HTTP_FORBIDDEN);
+
+        return $next($request);
+    }
+}
