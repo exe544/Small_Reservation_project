cb808766d7f724209170ac4c7eb83c9437f4d971
exe544 <a.lashko97@gmail.com>
2023-11-30 23:44:38 +0200

    create crud for activities

---
+++ app/Http/Requests/Activity/ActivityUpdateRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Activity;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ActivityUpdateRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:3', 'max:255'],
+            'description' => ['required', 'min:3', 'max:1000'],
+            'start_date' => ['required', 'date'],
+            'price' => ['required', 'numeric', 'min:1'],
+            'image' => ['image', 'nullable', 'max:1024'],
+            'guide_id' => ['required', Rule::exists('users', 'id')],
+        ];
+    }
+}
