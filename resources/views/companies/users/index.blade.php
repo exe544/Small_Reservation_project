1dbd157b21b9adf6a5fa00d2caaf97d2df425f13
exe544 <a.lashko97@gmail.com>
2023-12-03 18:14:26 +0200

    add RegistrationInvitation model and migration

--- resources/views/companies/users/index.blade.php
+++ resources/views/companies/users/index.blade.php
@@ -11,7 +11,7 @@
 
                         <a href="{{ route('companies.users.create', $company) }}"
                            class="mb-4 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-xs font-semibold uppercase tracking-widest text-gray-700 shadow-sm transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25">
-                            Create
+                            Invite new user
                         </a>
 
                         <div class="min-w-full align-middle">
