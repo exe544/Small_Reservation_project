c0ecc9984f8b205048ef19df7b12c9f3f7c37bb3
exe544 <a.lashko97@gmail.com>
2023-12-08 01:59:35 +0200

    add cancel activity feature and tests

--- app/Http/Controllers/MyActivityController.php
+++ app/Http/Controllers/MyActivityController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class MyActivityController extends Controller
 {
     public function show(): View
     {
-        return view('activities.my-activities');
+        $activities = Auth::user()->activities()->orderBy('start_date')->paginate(9);
+        return view('activities.my-activities', compact('activities'));
+    }
+
+    public function destroy(Activity $activity): RedirectResponse
+    {
+        abort_if(! Auth::user()->activities->contains($activity), 403);
+        Auth::user()->activities()->detach($activity);
+
+        return to_route('my-activity.show')->with('success', 'Activity removed!');
     }
 }
